13b17cd508b0fce3830dd38e10635d5a597bd2aa
SarojKumarRanjan <sarojranjan555@gmail.com>
2025-02-10 01:13:02 +0530

    complete for GET req

--- proxyserver.c
+++ proxyserver.c
@@ -161,6 +161,210 @@ cache_element* head;
 int cache_size; // this will denote the current size of the cache
 
 
+ //this function will check the http version of the request
+int checkHTTPversion(char* version){
+  if(strcmp(version,"HTTP/1.0")==0 || strcmp(version,"HTTP/1.1")==0){
+    return 1;
+  }
+  return 0;
+}
+
+/*
+function for the errormessage
+->this function will send the error message to the client
+->it will take the socketId of the client
+->it will take the error code
+
+*/
+
+int sendErrorMessage(int socketId,int error_code){
+  char str[1024];
+  char current_time[50];
+  time_t now = time(0);
+
+  struct tm tm = *gmtime(&now);
+  strftime(current_time,sizeof(current_time),"%a, %d %b %Y %H:%M:%S %Z",&tm);
+
+  switch (error_code)
+  {
+  case 400:
+    sprintf(str,"HTTP/1.1 400 Bad Request\r\nDate: %s\r\nServer: Proxy-Server\r\nContent-Length: 0\r\nConnection: close\r\n\r\n",current_time);
+    send(socketId,str,strlen(str),0);
+    break;
+  
+  case 500:
+    sprintf(str,"HTTP/1.1 500 Internal Server Error\r\nDate: %s\r\nServer: Proxy-Server\r\nContent-Length: 0\r\nConnection: close\r\n\r\n",current_time);
+    send(socketId,str,strlen(str),0);
+    break;
+
+  case 501:
+    sprintf(str,"HTTP/1.1 501 Not Implemented\r\nDate: %s\r\nServer: Proxy-Server\r\nContent-Length: 0\r\nConnection: close\r\n\r\n",current_time);
+    send(socketId,str,strlen(str),0);
+    break;
+  
+  case 403:
+     sprintf(str,"HTTP/1.1 403 Forbidden\r\nDate: %s\r\nServer: Proxy-Server\r\nContent-Length: 0\r\nConnection: close\r\n\r\n",current_time);
+    send(socketId,str,strlen(str),0);
+    break;
+
+  case 404:
+    sprintf(str,"HTTP/1.1 404 Not Found\r\nDate: %s\r\nServer: Proxy-Server\r\nContent-Length: 0\r\nConnection: close\r\n\r\n",current_time);
+    send(socketId,str,strlen(str),0);
+    break;
+  
+  default:
+    break;
+  
+  
+}
+}
+
+
+/*
+this function connects to the remote server
+-> remote server is the server to which we have to make the request
+-> it will take the host address and the port number as the parameter
+-> it will return the socketId of the remote server
+
+*/
+int connect_to_remote_server( char* host_address, int port_number){
+
+  int remote_socketId = socket(AF_INET,SOCK_STREAM,0);
+
+  if(remote_socketId<0){
+    perror("Failed to create the socket\n");
+    return -1;
+  }
+
+  struct sockaddr_in remote_add;
+
+  bzero((char*)&remote_add,sizeof(remote_add));
+
+  remote_add.sin_family = AF_INET;
+  remote_add.sin_port = htons(port_number);
+
+  struct hostent *server = gethostbyname(host_address);
+
+  if(server==NULL){
+    perror("Failed to get the host\n");
+    return -1;
+  }
+
+  bcopy((char*)&server->h_addr_list[0],(char*)&remote_add.sin_addr.s_addr,server->h_length);
+
+  if(connect(remote_socketId,(struct sockaddr*)&remote_add,sizeof(remote_add))<0){
+    perror("Failed to connect to the remote server\n");
+    return -1;
+  }
+
+  return remote_socketId;
+
+}
+
+
+/*
+this function handle the request of the client
+-> it will take the socketId of the client
+-> it will take the request of the client
+-> it will take the tempRequest of the client
+-> it will return the bytes send to the client
+-> it will return -1 if the request is not handled properly
+
+
+*/
+
+int handle_request(int socketId,ParsedRequest* request,char* tempRequest){
+
+  char* buf = (char*)malloc(MAX_BYTES*sizeof(char));
+  strcpy(buf,"GET");
+  strcat(buf,request->path);
+  strcat(buf," ");
+  strcat(buf,request->version);
+  strcat(buf,"\r\n");
+
+  size_t len = strlen(buf);
+
+  if(ParsedHeader_set(request,"Host",request->host)<0){
+    printf("Failed to set the header\n");
+    return -1;
+  }
+
+  if(ParsedHeader_set(request,"Connection","close")<0){
+    printf("Failed to set the header\n");
+    return -1;
+  }
+
+  if(ParsedRequest_unparse_headers(request,buf+len,MAX_BYTES-len)<0){
+    printf("Failed to unparse the headers\n");
+    return -1;
+  }
+
+  int server_port = 80;
+  if(request->host!=NULL){
+    server_port = atoi(request->port);
+
+    
+  }
+
+int remote_socketId = connect_to_remote_server(request->host,server_port);
+
+if(remote_socketId<0){
+  printf("Failed to connect to the remote server\n");
+  return -1;
+
+}
+
+int bytes_send_client = send(remote_socketId,buf,strlen(buf),0);
+bzero(buf,MAX_BYTES);
+
+
+
+ bytes_send_client = recv(remote_socketId,buf,MAX_BYTES-1,0);
+
+
+
+char* tempbuffer = (char*)malloc(MAX_BYTES*sizeof(char));
+
+
+int tempbuffersize = MAX_BYTES;
+int temp_buffer_index = 0;
+
+while(bytes_send_client>0){
+  
+  bytes_send_client = send(socketId,buf,strlen(buf),0);
+
+  for(int i=0;i<bytes_send_client/sizeof(char);i++){
+    tempbuffer[temp_buffer_index] = buf[i];
+    temp_buffer_index++;
+  }
+  tempbuffersize += MAX_BYTES;
+  tempbuffer = (char*)realloc(tempbuffer,tempbuffersize*sizeof(char));
+
+if(bytes_send_client<0){
+  perror("Failed to send the data to the client server\n");
+  break;
+}
+
+bzero(buf,MAX_BYTES);
+
+
+bytes_send_client = recv(remote_socketId,buf,MAX_BYTES-1,0);
+
+
+ 
+}
+
+tempbuffer[temp_buffer_index] = '\0';
+free(buf);
+add_cache_element(tempRequest,str(tempbuffer),tempbuffer);
+free(tempbuffer);
+close(remote_socketId);
+
+return 0;
+
+
+}
+
 void *thread_fn(void *socket_new){
 
   sem_wait(&semaphore);//this will wait until the semaphore value is greater than 0
@@ -240,7 +444,55 @@ if(temp!=NULL){
 
 
 
+}else if(bytes_send_client > 0){
+  length = strlen(buffer);
+  //parse the request
+  ParsedRequest* request = ParsedRequest_create();
+
+  if(ParsedRequest_parse(request,buffer,length)<0){
+    printf("Parsing failed\n");
+  }else{
+    bzero(buffer,MAX_BYTES);
+    //get the method of the request
+    //if the method is GET then we will send the request to the server
+    //currently we are not handling the POST,PUT,DELETE methods
+    if(!strcmp(request->method,"GET")){
+
+        if(request->host && request->path && checkHTTPversion(request->version)==1){
+          bytes_send_client = handle_request(socketId,request,tempRequest);
+          if(bytes_send_client==-1){
+            sendErrorMessage(socketId,500);
+          }
+        }else{
+          sendErrorMessage(socketId,500);
+        }
+    }
+    else{
+      printf("Only GET method is allowed\n");
+    }
+  }
+
+  ParsedRequest_destroy(request);
+
+
 }
+
+else if(bytes_send_client == 0){
+  printf("Client disconnected\n");
+
+}
+shutdown(socketId,SHUT_RDWR);
+close(socketId);
+free(buffer);
+sem_post(&semaphore);
+
+sem_getvalue(&semaphore,&p);
+
+printf("Semaphore post value is %d\n",p);
+
+free(tempRequest);
+
+return NULL; 
 }
 
 int main(int argc , char* argv[]){
@@ -348,7 +600,78 @@ return 0;
 
 
 
+cache_element* find(char* url){
 
+  cache_element* temp = NULL;
+
+  int time_lock_val = pthread_mutex_lock(&lock);
+
+  printf("Time lock value is %d\n",time_lock_val);
+
+   temp = head;
+  while(temp!=NULL){
+    if(strcmp(temp->url,url)==0){
+      temp->last_used_time = time(0);
+      return temp;
+    }
+    temp = temp->next;
+  }
+  return NULL;
+}
+
+
+cache_element* create_cache_element(char* url,int size,char* data){
+
+  int time_lock_val = pthread_mutex_lock(&lock);
+
+  printf("Create cahe  Time lock value is %d\n",time_lock_val);
+
+  cache_element* temp = (cache_element*)malloc(sizeof(cache_element));
+  temp->url = (char*)malloc(strlen(url)*sizeof(char)+1);
+  strcpy(temp->url,url);
+  temp->data = (char*)malloc(size*sizeof(char)+1);
+  strcpy(temp->data,data);
+  temp->len = size;
+  temp->last_used_time = time(0);
+  temp->next = NULL;
+  return temp;
+}
+
+
+void remove_cache_element(){
+
+  int time_lock_val = pthread_mutex_lock(&lock);
+
+  printf("Remove cache Time lock value is %d\n",time_lock_val);
+
+  cache_element* temp = head;
+  cache_element* prev = NULL;
+  cache_element* min = NULL;
+  time_t min_time = time(0);
+
+  while(temp!=NULL){
+    if(temp->last_used_time<min_time){
+      min_time = temp->last_used_time;
+      min = temp;
+      prev = temp;
+      temp = temp->next;
+    }else{
+      prev = temp;
+      temp = temp->next;
+    }
+  }
+
+  if(min!=NULL){
+    if(min==head){
+      head = head->next;
+    }else{
+      prev->next = min->next;
+    }
+    free(min->url);
+    free(min->data);
+    free(min);
+  }
+}
 
 
 
